eefa0958a07d39a0b90494e82da50fbf3da236dc
Paul Bakeman <100813108+pandmbake@users.noreply.github.com>
2022-06-11 16:25:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-
+function toggleImage() {
+  var x = document.getElementById("bigfoot");
+  if (x.style.display === "none") {
+    x.style.display = "inline";
+  } else {
+    x.style.display = "none";
+  }
+  var y = document.getElementById('forest');
+  if  (y.style.display === 'inline') {
+    y.style.display = 'none';
+  } else {
+    y.style.display = 'inline';
+  }
+    }
